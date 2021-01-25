2fc786a729f58ca858813c1a58a5a67f34c4d1bc
yuya <siren.night43@gmail.com>
2021-01-26 06:14:57 +0900

    カードデザインの適用

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,54 @@
 <template>
   <div id="app">
-    <div class="container"></div>
+    <div class="container">
+      <ul class="item-list">
+        <li v-for="(item, index) in cookingRecords" v-bind:key="index" class="item">
+          <CookingRecord :imageUrl="item.image_url" :comment="item.comment" :recipeType="item.recipe_type" :recordedAt="item.recorded_at"/>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import 'normalize.css';
+import axios from 'axios';
+import CookingRecord from './components/CookingRecord';
+
 export default {
   name: 'App',
+  components: { CookingRecord },
+  data() {
+    return {
+      cookingRecords: []
+    }
+  },
+  created() {
+    axios.get('/cooking_records').then(response => {
+      console.log(response.data)
+      this.cookingRecords = response.data['cooking_records'];
+    })
+  }
 }
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.container {
+  display: flex;
+  width: 100vw;
+  background-image: url('assets/images/cork.jpg');
+}
+
+.item-list {
+  padding: 0;
+}
+
+.item {
+  margin: 2rem;
+  list-style: none;
 }
 </style>
---
+++ src/components/CookingRecord.vue
@@ -0,0 +1,72 @@
+<template>
+  <div :class="['cooking-record', recipeType]">
+    <p class="recorded-time">{{ recordedAt | moment() }}</p>
+    <div class="content">
+      <img :src="imageUrl" class="record-image"/>
+      <p>{{ comment }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  name: 'CookingRecord',
+  props: {
+    imageUrl: String,
+    comment: String,
+    recipeType: String,
+    recordedAt: String
+  },
+  filters: {
+    moment(value) {
+      return moment(value).format('YYYY.MM.DD');
+    }
+  }
+}
+</script>
+
+<style scoped>
+@font-face {
+  font-family: 'karakaze';
+  src: url('../assets/fonts/ShigotoMemogaki-Regular-1-01.ttf');
+}
+.cooking-record {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  border-radius: 0.5rem;
+  box-shadow: 2px 2px 3px;
+}
+
+.content {
+  width: 80%;
+  margin: 0 auto 1rem;
+  padding: 0 1rem;
+  font-size: 1.2rem;
+  background-color: rgba(255, 255, 255,0.5);
+  border-radius: 0.5rem;
+}
+
+.recorded-time {
+  margin-left: 1rem;
+}
+.record-image {
+  width: 100%;
+  margin: 1rem 0 0;
+}
+
+.main_dish {
+  background-image: url('../assets/images/main_dish.png');
+}
+
+.side_dish {
+  background-image: url('../assets/images/side_dish.png');
+}
+
+.soup {
+  background-image: url('../assets/images/soup.png');
+}
+</style>
